6eac723a3a7339a912333bb3cd97b48a3a285558
Jules Claussen <jules.claussen@etu.imt-lille-douai.fr>
2018-12-04 15:15:49 +0100

    Modif script post slack
    Signed-off-by: Jules Claussen <jules.claussen@etu.imt-lille-douai.fr>

--- httpSenderServiceCustom.py
+++ httpSenderServiceCustom.py
@@ -5,16 +5,23 @@ import sys
 
 app = Flask(__name__)
 
-url = sys.argv[1]
-message = sys.argv[2]
-payload = "{\r\n    \"text\": \""+message+"\"\r\n}"
-
-headers = {
-    'Content-Type': "application/json",
-    'cache-control': "no-cache",
-    'Postman-Token': "f7f5d4a0-8b39-4283-9806-67f233e54468"
-    }
-
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-	response = requests.request("POST", url, data=payload, headers=headers)
+	if request.method == 'POST':
+		content = request.json
+		url = content['url']
+		message = content['message']
+		payload = "{\r\n    \"text\": \""+message+"\"\r\n}"
+		headers = {
+			'Content-Type': "application/json",
+			'cache-control': "no-cache",
+			'Postman-Token': "f7f5d4a0-8b39-4283-9806-67f233e54468"
+		}
+		response = requests.request("POST", url, data=payload, headers=headers)
+		return reponse.text
+	else:
+		return 'Hello World : GET REQUEST'
+
+
+
+	
